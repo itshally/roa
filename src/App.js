083f3a50362cf199a52c1681e41b292c37d02737
Hally <hbianan@gmail.com>
2021-01-18 17:23:20 -0500

    updated/added dependencies and main files, changed favicon

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import { NavigationBar, Home, About, Ranking, Seasonal, Character, Footer } from './components'
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "react-datepicker/dist/react-datepicker.css";
+
+import './assets/styles/navbar.css'
+import './assets/styles/home.css'
+import './assets/styles/about.css'
+import './assets/styles/ranking.css'
+import './assets/styles/character.css'
+import './assets/styles/seasonal.css'
+import './assets/styles/footer.css'
+
+const App = () => {
+    return (
+        <div className="App">
+            <Router>
+                <NavigationBar/>
+                <div className="body-container">
+                    <Switch>
+                        <Route exact path="/" component={Home}/>
+                        <Route exact path="/about" component={About}/>
+                        <Route exact path="/seasonal" component={Seasonal}/>
+                        <Route exact path="/:top" component={Ranking}/>
+                        <Route exact path="/character/:id" component={Character}/>
+                    </Switch>
+                </div>
+            </Router>
+            <Footer/>
+        </div>
+    )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -2,16 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
 import reportWebVitals from './reportWebVitals';
 
+/* -------------------------------- for redux ------------------------------- */
+import reduxThunk from "redux-thunk";
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import reducers from './reducers'
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(reducers, composeEnhancers(applyMiddleware(reduxThunk)))
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
